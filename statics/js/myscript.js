30f701cd92499344abc266558cb532f5527a74f8
Chloe <chloe60492@gmail.com>
2023-02-25 12:43:40 +0800

    加上彈跳視窗的效果

--- statics/js/myscript.js
+++ statics/js/myscript.js
@@ -1,3 +1,22 @@
 $(document).ready(function(){
-   
+//    popup的書籤
+    $(".popup-btn").click(function() {
+        var href = $(this).attr("href")
+        $(href).show(250);
+        $(href).children$("popup-box").removeClass("transform-out").addClass("transform-in");
+        e.preventDefault();
+    });
+    
+    $(".popup-close").click(function() {
+        closeWindow();
+    });
+    // $(".popup-wrap").click(function(){
+    //   closeWindow();
+    // })
+    function closeWindow(){
+        $(".popup-wrap").hide(200);
+        $(".popup-box").removeClass("transform-in").addClass("transform-out");
+        event.preventDefault();
+    }
+//    書籤結束
 });
