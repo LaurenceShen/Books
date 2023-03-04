b6884c644549b08b9a3ce621cfcad02ff5335555
mincih <mincih.chou@gmail.com>
2023-03-04 22:43:50 +0800

    some change

--- statics/js/map.js
+++ statics/js/map.js
@@ -1,6 +1,6 @@
 $(".popup-btn").click(function() {
     var href = $(this).attr("href")
-    $(href).fadeIn(250);
+    $(href).toggle(250);
     $(href).children$("popup-box").removeClass("transform-out").addClass("transform-in");
     e.preventDefault();
   });
@@ -12,7 +12,7 @@ $(".popup-btn").click(function() {
   //   closeWindow();
   // })
   function closeWindow(){
-    $(".popup-wrap").fadeOut(200);
+    $(".popup-wrap").toggle(200);
     $(".popup-box").removeClass("transform-in").addClass("transform-out");
-    event.preventDefault();
+    Event.preventDefault();
   }
