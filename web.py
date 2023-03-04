032c42fd598cd6d395b4204ad5e2b1df0c27078a
LaurenceShen <laurence220016@gmail.com>
2023-03-04 20:25:50 +0800

    .

--- web.py
+++ web.py
@@ -31,13 +31,22 @@ cursor.execute("SELECT * FROM User;")
 users = cursor.fetchall()
 cursor.close()
 conn.close()
-#users = {'Me': {'password': 'myself'}}
-'''
-=======
-books = [['A', 'Allen', '借閱中', '2050/01/01', '25%', '50'], 'B', 'C']
-users = {'Me': {'password': 'myself'}}
->>>>>>> origin/main
-'''
+
+Month = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
+borrowed = {}
+conn = sqlite3.connect('Coding101.db')
+cursor = conn.cursor()
+for i in Month:
+    sql = """
+SELECT * FROM Borrowed_{} INNER JOIN User ON User.ID = Borrowed_{}.User_ID
+    """.format(i, i)
+    cursor.execute(sql)
+    borrowed_mon = cursor.fetchall()
+    borrowed[i] = borrowed_mon
+cursor.close()
+conn.close()
+
+
 pjdir = os.path.abspath(os.path.dirname(__file__))
 
 app = Flask(__name__, static_folder="statics", static_url_path="/")
@@ -137,7 +146,8 @@ def note(book):
         #print(i[0])
         if i[0] == int(book):
             output = i
-            print(':(')
+    if (output == None):
+        return render_template('error.html')
     return render_template('noteindex.html', book = output)
 
 @app.route('/logout')
