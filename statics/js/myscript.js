01df16172a3945239d4bc845bc59a0e5cf4e158f
Chloe <chloe60492@gmail.com>
2023-03-04 20:24:45 +0800

    改變數名稱

--- statics/js/myscript.js
+++ statics/js/myscript.js
@@ -22,7 +22,28 @@ $(document).ready(function(){
         $(".popup-box").removeClass("transform-in").addClass("transform-out");
         event.preventDefault();
     }
-//    書籤結束
+//  書籤結束
 
+});
 
+//  progressbar
+const progress = document.querySelector(".progress-done");
+const input = document.querySelector(".input");
+const maxInput = document.querySelector(".maxInput");
+let finalValue = 0;
+let max = 0;
+
+function changeWidth(){
+    progress.style.width = `${(finalValue / max) * 100}%`;
+    progress.innerText = `${Math.ceil((finalValue / max) * 100)}%`;
+}
+
+input.addEventListener("keyup", function(){
+    finalValue = parseInt(input.value, 10);
+    changeWidth();
+});
+
+maxInput.addEventListener("keyup", function(){
+    max = parseInt(maxInput.value, 10);
+    changeWidth();
 });
