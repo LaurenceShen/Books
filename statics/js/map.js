aa081f161df0f2dcf94eb8f8598aa2dfc34202d0
mincih <mincih.chou@gmail.com>
2023-03-06 19:48:29 +0800

    map pop up

--- statics/js/map.js
+++ statics/js/map.js
@@ -1,18 +1,72 @@
-$(".popup-btn").click(function() {
-    var href = $(this).attr("href")
-    $(href).toggle(250);
-    $(href).children$("popup-box").removeClass("transform-out").addClass("transform-in");
-    e.preventDefault();
-  });
-  
-  $(".popup-close").click(function() {
-    closeWindow();
-  });
-  // $(".popup-wrap").click(function(){
-  //   closeWindow();
-  // })
-  function closeWindow(){
-    $(".popup-wrap").toggle(200);
-    $(".popup-box").removeClass("transform-in").addClass("transform-out");
-    Event.preventDefault();
-  }
+function openPopup2() {
+  var map_popup = document.getElementById("map_popup");
+  map_popup.style.display = "flex";
+}
+function closePopup2() {
+  var map_popup = document.getElementById("map_popup");
+  map_popup.style.display = "none";
+}
+
+
+  function openPopup() {
+  var popup = document.getElementById("popup1");
+  popup.style.display = "flex";
+}
+
+function closePopup() {
+  var popup6 = document.getElementById("popup6");
+  var popup5 = document.getElementById("popup5");
+  popup6.style.display = "none";
+  popup5.style.display = "none";
+}
+
+function openNextPopup1() {
+  var popup = document.getElementById("popup1");
+  popup.style.display = "none";
+  var popup2 = document.getElementById("popup2");
+  popup2.style.display = "flex";
+}
+function openNextPopup2() {
+  var popup = document.getElementById("popup1");
+  popup.style.display = "none";
+  var popup2 = document.getElementById("popup2");
+  popup2.style.display = "none";
+  var popup3 = document.getElementById("popup3");
+  popup3.style.display = "flex";
+}
+function openNextPopup3() {
+  var popup = document.getElementById("popup1");
+  popup.style.display = "none";
+  var popup2 = document.getElementById("popup2");
+  popup2.style.display = "none";
+  var popup3 = document.getElementById("popup3");
+  popup3.style.display = "none";
+  var popup4 = document.getElementById("popup4");
+  popup4.style.display = "flex";
+}
+function openNextPopup4() {
+  var popup = document.getElementById("popup1");
+  popup.style.display = "none";
+  var popup2 = document.getElementById("popup2");
+  popup2.style.display = "none";
+  var popup3 = document.getElementById("popup3");
+  popup3.style.display = "none";
+  var popup4 = document.getElementById("popup4");
+  popup4.style.display = "none";
+  var popup5 = document.getElementById("popup5");
+  popup5.style.display = "flex";
+}
+function openNextPopup5() {
+  var popup = document.getElementById("popup1");
+  popup.style.display = "none";
+  var popup2 = document.getElementById("popup2");
+  popup2.style.display = "none";
+  var popup3 = document.getElementById("popup3");
+  popup3.style.display = "none";
+  var popup4 = document.getElementById("popup4");
+  popup4.style.display = "none";
+  var popup5 = document.getElementById("popup5");
+  popup5.style.display = "none";
+  var popup6 = document.getElementById("popup6");
+  popup6.style.display = "flex";
+}
